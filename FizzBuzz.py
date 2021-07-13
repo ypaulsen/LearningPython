03f961d5983b2fe8a05c139c07434707a5ad2917
ypaulsen <55412133+ypaulsen@users.noreply.github.com>
2021-07-13 12:43:04 -0700

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,13 @@
-x = [1,2,3,4]
-y = list(range(1, 101))
-z = [x%3 for x in y]
-print(z.index(0))
+x = list(range(1, 101))
+
+for i in range(len(x)):
+    if x[i] % 15 == 0:
+        x[i] = "FizzBuzz"
+    else:
+        if x[i] % 3 == 0:
+            x[i] = "Fizz"
+        else:
+            if x[i] % 5 ==0:
+                x[i] = "Buzz"
 
 print(x)
-print(y)
-print(z)
