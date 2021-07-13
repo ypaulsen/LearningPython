4de42935a25e20f2f225ef08bcd0a7a555fe5896
ypaulsen <55412133+ypaulsen@users.noreply.github.com>
2021-07-12 23:35:52 -0700

    Create While Loops.py

---
+++ While Loops.py
@@ -0,0 +1,11 @@
+
+i=1
+while i <= 10:
+    print(i)
+    i=i+1      # There is a shortcut in python. The code chunk below functions exactly the same as this one.
+
+while i <= 10:
+    print(i)
+    i += 1
+print("Loop over")
+
