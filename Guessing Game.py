f9b0b87df93b200bec3c134de1ea0f3b78015fa6
ypaulsen <55412133+ypaulsen@users.noreply.github.com>
2021-07-12 23:38:07 -0700

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,11 +1,13 @@
 # A guessing game
 
+# Set parameters
 secret_word = "telescope"
 guess = ""
 guess_count = 0
 guess_limit = 3
 out_of_guesses = False
 
+# while loop checks conditions
 while guess != secret_word and not(out_of_guesses):
     if guess_count < guess_limit:
         guess = input("What is your guess?")
@@ -13,8 +15,8 @@ while guess != secret_word and not(out_of_guesses):
     else:
         out_of_guesses = True
 
+# only two ways out of the while loop:
 if out_of_guesses:
     print("Out of Guesses!")
 else:
     print("Winner!")
-
