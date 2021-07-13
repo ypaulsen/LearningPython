363eb62fc87e43020835bf50bd3473a3232e4628
ypaulsen <55412133+ypaulsen@users.noreply.github.com>
2021-07-12 23:49:36 -0700

    Create For loops.py

---
+++ For loops.py
@@ -0,0 +1,19 @@
+# EG
+for letter in "Dobsonian Telescope":
+    print(letter)
+
+friends = ["Jim", "Pam", "Kevin"]
+for name in friends:
+    print(name)
+
+for i in range(10):
+    print(i)
+
+for i in range(len(friends)):
+    print(friends[i])
+
+for i in range(5):
+    if i == 0:
+        print("first iteration")
+    else:
+        print("not first")
